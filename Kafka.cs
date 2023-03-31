795ff8d3fb70fa369611ad2f888a2a35d0adfd65
Konstantin Kirdyanov <k.kirdyanov@smartway.today>
2023-03-31 16:25:29 +0300

    Init

---
+++ Kafka.cs
@@ -0,0 +1,68 @@
+﻿using Confluent.Kafka;
+
+namespace TrainingApp
+{
+    public static class Kafka
+    {
+        const string address = "localhost:29092";
+        const string topic = "new-topic";
+        public static async Task AddMess(string mes)
+        {
+            var config = new ProducerConfig { BootstrapServers = address };
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var topicPartition = new TopicPartition(topic, new Partition(0));
+                    var dr = await p.ProduceAsync(topicPartition, new Message<Null, string> { Value = mes });
+                    Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+
+        public static void Reading(string groupId)
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = groupId,
+                BootstrapServers = address,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                c.Subscribe(topic);
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed '{cr.Value}' at: '{cr.TopicPartitionOffset}' group - {conf.GroupId} thread - {Thread.CurrentThread.ManagedThreadId}.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using TrainingApp;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Task.Run(() => Kafka.Reading("g1"));
+        Task.Run(() => Kafka.Reading("g1"));
+        Task.Run(() => Kafka.Reading("g2"));
+
+        for (int i = 0; i < 500; i++)
+        {
+            await Kafka.AddMess($"mes #{i}");
+            Thread.Sleep(1000);
+        }
+
+        Console.ReadLine();
+    }
+}
